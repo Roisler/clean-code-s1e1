7b42831a74be42c6f9829acc8368ff33448be690
Roisler <roislerxs@gmail.com>
2024-01-08 19:18:45 +0700

    refactor(advanced-1.2): add alt atributes for all images in index.html. Add alt attributes for all images in app.js

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.classList.add('delete');
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.classList.add('image');
+    deleteButtonImg.setAttribute('alt', 'remove icon');
     deleteButton.appendChild(deleteButtonImg);
 
 
